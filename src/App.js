0626123e564ad7443a95c789f264a8d70b10b3c1
Noah Edelstein <Noedelstein@gmail.com>
2019-12-07 20:57:41 -0500

    added image value to position object

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 import * as PIXI from "pixi.js";
 import { Viewport } from "pixi-viewport";
 
-let positions;
+import { images, positions } from "./sotu_umap.js";
+
+// let positions;
 
 function App() {
   const canvas = useRef(null);
@@ -12,103 +14,103 @@ function App() {
 
   // load custer pos onto canvas
   useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/Nedelstein/designing-ml-final/master/sotu_umap_position.json"
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        positions = data;
-        console.log(positions);
-        const app = new PIXI.Application({
-          width: window.innerWidth,
-          height: window.innerHeight,
-          view: canvas.current
-        });
-        document.body.appendChild(app.view);
+    console.log(positions);
+    // console.log(images);
+    const app = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      view: canvas.current
+    });
+    document.body.appendChild(app.view);
 
-        let positionDict = {};
+    let positionDict = {};
 
-        // make photo lookup and tie it to sotu json object
-        //loop through photo lookup and load image keyed to pres name
-        const lincoln =
-          "https://raw.githubusercontent.com/Nedelstein/designing-ml-final-react-app/master/resized/resized_president_imgs_01-washington.jpg";
+    // make photo lookup and tie it to sotu json object
+    //loop through photo lookup and load image keyed to pres name
+    const lincoln =
+      "https://raw.githubusercontent.com/Nedelstein/designing-ml-final-react-app/master/resized/resized_president_imgs_01-washington.jpg";
 
-        app.loader.add("Lincoln", lincoln);
-        for (let i = 0; i < positions.length; i++) {
-          const presName = positions[i].President;
+    app.loader.add("Lincoln", lincoln);
+    for (let i = 0; i < positions.length; i++) {
+      for (let j = 0; j < images.length; j++) {
+        const presName = positions[i].President;
+        const date = positions[i].Date;
+        const image = images[j].image;
 
-          //   const date = positions[i].Date;
-          //   app.loader.add(presName, lincoln);
-
-          positionDict[presName] = positions[i];
-          // console.log("pres name", presName);
+        //add image value to position object
+        if (presName == images[j].President) {
+          positions[i].image = image;
         }
-        const viewport = new Viewport({
-          screenWidth: window.innerWidth,
-          screenHeight: window.innerHeight,
-          worldWidth: positions.length,
-          worldHeight: positions.length,
-          interaction: app.renderer.plugins.interaction
+
+        app.loader.add(presName);
+
+        positionDict[presName] = positions[i];
+      }
+    }
+    const viewport = new Viewport({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      worldWidth: positions.length,
+      worldHeight: positions.length,
+      interaction: app.renderer.plugins.interaction
+    });
+
+    // add viewport to stage
+    app.stage.addChild(viewport);
+
+    app.renderer.backgroundColor = 0xff00cc;
+    // interaction plugins
+    viewport
+      .drag()
+      .pinch()
+      .wheel()
+      .decelerate();
+
+    app.loader.load((loader, resources) => {
+      // console.log(resources);
+      console.log("app dims:", app.renderer.width, app.renderer.height);
+      // console.log(app.renderer.backgroundColor);
+
+      //this is where you loop through your SOTU addresses
+      for (let key in positions) {
+        const imageSprite = new PIXI.Sprite(resources["Lincoln"].texture);
+        // const cluster_pos = positions[key].Cluster_Pos;
+
+        // imageSprite.x = app.renderer.width * (cluster_pos[0] * 2 - 1);
+        // imageSprite.y = app.renderer.width * (cluster_pos[1] * 2 - 1);
+
+        imageSprite.x = app.renderer.width * 0.4;
+        imageSprite.y = app.renderer.height * 0.4;
+
+        imageSprite.anchor.x = 0.5;
+        imageSprite.anchor.y = 0.5;
+
+        imageSprite.interactive = true;
+        // console.log("img sprite xy:", imageSprite.x, imageSprite.y);
+
+        const name = key;
+        imageSprite.on("click", () => {
+          setOverlay(positionDict[name]);
         });
 
-        // add viewport to stage
-        app.stage.addChild(viewport);
-
-        app.renderer.backgroundColor = 0xff00cc;
-        // interaction plugins
-        viewport
-          .drag()
-          .pinch()
-          .wheel()
-          .decelerate();
-
-        app.loader.load((loader, resources) => {
-          console.log(resources);
-          console.log("app dims:", app.renderer.width, app.renderer.height);
-          // console.log(app.renderer.backgroundColor);
-
-          //this is where you loop through your SOTU addresses
-          for (let key in positions) {
-            const imageSprite = new PIXI.Sprite(resources["Lincoln"].texture);
-            // const cluster_pos = positions[key].Cluster_Pos;
-
-            // imageSprite.x = app.renderer.width * (cluster_pos[0] * 2 - 1);
-            // imageSprite.y = app.renderer.width * (cluster_pos[1] * 2 - 1);
-
-            imageSprite.x = app.renderer.width * 0.4;
-            imageSprite.y = app.renderer.height * 0.4;
-
-            imageSprite.anchor.x = 0.5;
-            imageSprite.anchor.y = 0.5;
-
-            imageSprite.interactive = true;
-            console.log("img sprite xy:", imageSprite.x, imageSprite.y);
-
-            const name = key;
-            imageSprite.on("click", () => {
-              setOverlay(positionDict[name]);
-            });
-
-            imageSprite.on("mouseover", () => {
-              imageSprite.height = imageSprite.height * 2;
-              imageSprite.width = imageSprite.width * 2;
-            });
-
-            imageSprite.on("mouseout", () => {
-              imageSprite.height = imageSprite.height * 0.5;
-              imageSprite.width = imageSprite.width * 0.5;
-            });
-
-            viewport.addChild(imageSprite);
-          }
+        imageSprite.on("mouseover", () => {
+          imageSprite.height = imageSprite.height * 2;
+          imageSprite.width = imageSprite.width * 2;
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+
+        imageSprite.on("mouseout", () => {
+          imageSprite.height = imageSprite.height * 0.5;
+          imageSprite.width = imageSprite.width * 0.5;
+        });
+
+        viewport.addChild(imageSprite);
+      }
+    });
+  });
+  // .catch(err => {
+  //   console.log(err);
+  // });
+  // }, []);
 
   return (
     <div className="App">
